92c331255526d85a3e05d374aa3533192dc85670
cowboy8625 <cowboy8625@protonmail.com>
2022-02-09 11:34:55 -0600

    Fix for scrolling right

--- crates/revi-core/src/window.rs
+++ crates/revi-core/src/window.rs
@@ -254,15 +254,6 @@ impl Window {
     pub fn scroll_right(&mut self, cols: usize) {
         self.scroll_offset.add_to_x(cols);
         self.adjust_cursor_x()
-        // if cols + self.scroll_offset.as_usize_x() + self.cursor.as_usize_x()
-        //     < self
-        //         .buffer
-        //         .borrow()
-        //         .line_len(self.cursor_file().as_usize_y())
-        // {
-        //     self.scroll_offset.add_to_x(cols);
-        //     // self.adjust_cursor_x()
-        // }
     }
 
     pub fn insert_newline(&mut self) {
