28c3fde45856d900820a5e28ab52357e9e667dc1
cowboy8625 <cowboy8625@protonmail.com>
2022-02-20 16:52:41 -0600

    Fixed spelling errors.

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 -- mapper:nmap("g", {"yy", "p", "P"})
 -- mapper:map("Insert", "<C-a>", "DeleteChar")
 
--- This brote to my attension how limiting the current key engine is.
--- No custom commands can be sent in "Insert" mode.
+--This brought to my attention how limiting the current key engine is.
+--No custom commands can be sent in "Insert" mode.
 mapper:map(mode("Insert"), "<C-a>",{command("DeleteChar")})
--- No over lapping commands can be made so if you bind someththing to g then gg
--- will never be seen.
+--No over lapping commands can be made so if you bind something to `g` then `gg`
+--will never be seen.
 mapper:map(mode("Normal"), "g",{command("ScrollDown"), command("DeleteChar")})
 
 
