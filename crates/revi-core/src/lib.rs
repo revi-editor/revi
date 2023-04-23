3f085e37570a87b60b1c3e9fa88aba11e97cdbdf
cowboy8625 <cowboy8625@yahoo.com>
2023-04-22 20:59:04 -0500

    removed old code
    - deleted:    crates/revi-core/src/input.rs
    - modified:   crates/revi-core/src/lib.rs ***removed imports***
    - deleted:    crates/revi-core/src/position.rs

--- crates/revi-core/src/lib.rs
+++ crates/revi-core/src/lib.rs
@@ -6,25 +6,19 @@
 
 // mod command_bar;
 mod context;
-// mod input;
 mod pane;
 mod settings;
 // pub mod api;
 pub mod buffer;
 pub mod commands;
 mod key_parser;
-// pub mod line_number;
 pub mod mode;
-pub mod position;
-// pub mod revi;
-// mod text_formater;
 mod window;
 
 pub mod keymapper;
 
 pub use buffer::Buffer;
 pub use context::{Context, ContextBuilder};
-// pub use input::Input;
 pub use key_parser::KeyParser;
 pub use keymapper::Mapper;
 pub use mode::Mode;
