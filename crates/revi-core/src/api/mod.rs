fcbb588b719bd488e05eb086ef0067749b38a110
cowboy8625 <cowboy8625@yahoo.com>
2023-05-18 15:18:16 -0500

    made it so if no plugin path is provided revi will not crash

--- crates/revi-core/src/api/mod.rs
+++ crates/revi-core/src/api/mod.rs
@@ -19,14 +19,13 @@ impl Rhai {
         self.engine.eval_ast_with_scope::<T>(&mut self.scope, ast)
     }
 
-    pub fn compile(&mut self, filename: &str) -> Result<(), Box<EvalAltResult>> {
+    pub fn compile(&mut self, filename: &str) {
         if let Ok(ast) = self
             .engine
             .compile_file_with_scope(&self.scope, filename.into())
         {
             self.ast = ast;
         }
-        Ok(())
     }
 
     pub fn run_ast_with_scope(&mut self) -> Result<(), Box<EvalAltResult>> {
@@ -40,7 +39,7 @@ pub fn init(ctx: Context) -> Result<(), Box<EvalAltResult>> {
     let mut rhai = c.rhai.borrow_mut();
     rhai.engine.build_type::<ContextRhaiApi>();
     rhai.scope.push("revi", ContextRhaiApi(ctx));
-    rhai.compile("./userspace/init.rhai")?;
+    rhai.compile("./userspace/init.rhai");
     rhai.run_ast_with_scope()?;
     Ok(())
 }
